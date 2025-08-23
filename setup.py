31fe855c757aca7b913c281f5180b6279748ff59
LiftingVermosh <fallingstar233@163.com>
2025-08-23 16:13:30 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,35 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="scienceplotter",
+    version="0.1.0",
+    description="Scientific Plotting Tool for Researchers",
+    author="Vermosh",
+    packages=find_packages(exclude=['tests*']),
+    install_requires=[
+        'pandas>=1.0',
+        'matplotlib>=3.0',
+        'seaborn>=0.11',
+        'openpyxl>=3.0',   # 用于Excel处理
+        'Pillow>=8.0',     # 用于图像预览
+    ],
+    extras_require={
+        'test': [
+            'pytest>=6.0',
+            'pytest-cov>=2.0',
+            'coverage>=5.0',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'scienceplotter=cli_demo:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
