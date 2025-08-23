c0350a9ccff6c706a7189c6b98d51cc04058106a
LiftingVermosh <fallingstar233@163.com>
2025-08-23 16:34:42 +0800

    主程序入口

---
+++ main.py
@@ -0,0 +1,22 @@
+# main.py 
+# 应用入口
+
+import sys
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication
+import src.ui.main_window as main_window
+from src.core.font_manager import setup_chinese_font
+
+def main():
+    # 启用高DPI缩放
+    # QApplication.setAttribute(Qt.ApplicationAttribute.AA_EnableHighDpiScaling, True)
+    # QApplication.setAttribute(Qt.ApplicationAttribute.AA_UseHighDpiPixmaps, True)
+    app = QApplication(sys.argv)
+    window = main_window.MainWindow()
+    setup_chinese_font
+    sys.exit(app.exec())
+
+
+
+if __name__ == '__main__':
+    main()
